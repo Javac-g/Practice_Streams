129c6fce9a3d29c3da91e7862ad6f6cb5b3fce1b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-11 21:54:03 +0300

    seven

--- src/P_04_11_22/Six.java
+++ src/P_04_11_22/Six.java
@@ -1,6 +1,7 @@
 package P_04_11_22;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -8,6 +9,31 @@ public class Six {
 
 
     public static void main(String...args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(args[0]);
+        int i;
+        if( args.length != 1){
+
+            System.out.println("Enter file");
+
+        }
+
+        try {
+            do{
+               i = fileInputStream.read();
+
+               if(i != -1){
+                   System.out.print((char)i);
+               }
+            }while (i != -1);
+        }catch (IOException e){
+            System.out.println("read error");
+        }finally {
+
+
+            fileInputStream.close();
+
+
+        }
 
 
 
