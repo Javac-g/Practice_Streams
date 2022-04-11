7d7c34e498b09b47636b23289695b6f066956d72
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-11 20:01:13 +0300

    five

---
+++ src/P_04_11_22/Five.java
@@ -0,0 +1,14 @@
+package P_04_11_22;
+
+import java.io.PrintWriter;
+
+public class Five {
+
+    public static void main(String...args){
+
+        PrintWriter pr = new PrintWriter(System.out,true);
+        pr.println("Hello");
+
+
+    }
+}
