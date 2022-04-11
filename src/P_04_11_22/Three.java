245c23bf715f46e3b2294db3a1ee1aeb9cb69604
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-11 19:12:20 +0300

    Three

---
+++ src/P_04_11_22/Three.java
@@ -0,0 +1,34 @@
+package P_04_11_22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Three {
+    public static void main(String...args) throws IOException {
+
+        String[] data = new String[5];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int i = 0;
+        int j = 0;
+
+        do{
+            System.out.println("Enter: \n");
+
+            data[i] = br.readLine();
+
+        }while (i++ < 4 );
+
+        System.out.println("Result");
+
+
+        do{
+
+            System.out.println(data[j]);
+
+        }while (j++ < 4);
+
+    }
+}
