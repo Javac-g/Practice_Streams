08086e825b8a99b2375e54774f53eb375fb7e9a5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-11 21:37:47 +0300

    next one

---
+++ src/P_04_11_22/Six.java
@@ -0,0 +1,15 @@
+package P_04_11_22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Six {
+
+
+    public static void main(String...args) throws IOException {
+
+
+
+    }
+}
