bae06e1fc5cd876d7c7bf0a3675dab1fbae5aaa6
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-11 19:04:14 +0300

    three

--- src/P_04_11_22/Second.java
+++ src/P_04_11_22/Second.java
@@ -5,15 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Second {
+    static String processData(String str,String revers){
+
+        for (int i = str.length()-1; i >= 0; i--){
+
+            revers+=str.charAt(i);
+
+        }
+        return revers;
+    }
     public static void main(String...args) throws IOException {
 
         String str;
+        String revers = "";
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         do{
             System.out.println("Enter string: \n");
             str = br.readLine();
-            System.out.println(str);
+
+            System.out.println(processData(str,revers));
         }while (!str.equals("stop"));
     }
 }
