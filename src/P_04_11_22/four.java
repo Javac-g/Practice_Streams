fda64940f712c24aae254ff46d85cd9672074b8b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-11 19:57:38 +0300

    four

---
+++ src/P_04_11_22/four.java
@@ -0,0 +1,18 @@
+package P_04_11_22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class four {
+    public static void main(String...args) throws IOException {
+
+        int i;
+        i = 'k';
+        System.out.write(i);
+        System.out.write('\n');
+
+
+    }
+}
