0829ad83e64a16a801ee56c6b9893243f40a5e63
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-12 15:48:03 +0300

    Practice 04_12_22

---
+++ src/two/One.java
@@ -0,0 +1,16 @@
+package two;
+
+import java.io.File;
+
+public class One {
+    public static void main(String...args){
+
+        File f1 = new File("/");
+        File f2 = new File("/","autoexec.bat");
+        File f3 = new File(f1,"autoexec.bat");
+
+
+
+
+    }
+}
