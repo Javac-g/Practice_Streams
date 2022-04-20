5c369348b0bc8e6f1fa6a3c72d3c7f407fe669eb
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-20 22:46:55 +0300

    new

---
+++ src/three/One.java
@@ -0,0 +1,12 @@
+package three;
+
+import java.io.File;
+
+public class One {
+    public static void main(String...args){
+        File one = new File("/","rpg.txt");
+        System.out.println(one.exists());
+
+
+    }
+}
--- src/two/fourF.java
+++ src/two/fourF.java
@@ -4,11 +4,15 @@ import java.io.File;
 import java.io.FilenameFilter;
 
 public class fourF {
+
     public static void main(String...args){
 
         String dirname = "C:\\Users\\Max.000\\IdeaProjects\\Practice_Streams\\src\\two";
+
         File f1 = new File(dirname);
+
         FilenameFilter only = new four("java");
+
         String[] s = f1.list(only);
 
         for(int i = 0; i < s.length; i++){
@@ -19,4 +23,5 @@ public class fourF {
 
 
     }
+
 }
