60653becdfb75c64fb571f2cf2d814b5d067c453
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-21 21:51:24 +0300

    six

--- src/three/six.java
+++ src/three/six.java
@@ -1,4 +1,17 @@
 package three;
 
+import java.io.ByteArrayInputStream;
+
 public class six {
+     public static void main(String...args){
+
+         String s = "abcdefghjiklmnopqrstuxywvz";
+         byte[] b = s.getBytes();
+
+         ByteArrayInputStream one = new ByteArrayInputStream(b);
+         ByteArrayInputStream two = new ByteArrayInputStream(b,0,3);
+         System.out.println(b.length);
+
+     }
+
 }
