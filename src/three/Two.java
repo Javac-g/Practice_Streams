a5dd1089209908889343a415b6f5c7bccaa086d2
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-21 20:22:59 +0300

    04_21_22

--- src/three/Two.java
+++ src/three/Two.java
@@ -6,21 +6,23 @@ public class Two {
     public static void main(String...args){
         String dir = "C:\\Users\\Max.000\\IdeaProjects\\Practice_Streams\\src\\three";
         File f = new File(dir);
+
         if (f.isDirectory()){
+
             System.out.println("Catalog: " + dir);
             String[] s = f.list();
-            for (int i = 0 ; i < s.length; i++){
-                File f1 = new File(dir+ "/" + s[i]);
-                if(f1.isDirectory()){
-                    System.out.println(s[i] + " :catalog");
+
+            for (int i = 0; i <= s.length-1; i ++){
+                File f1 = new File(dir + "/" + s[i]);
+                if(f1.isDirectory()) {
+                    System.out.println("Catalog: " + s[i]);
                 }
-                else{
-                    System.out.println(s[i] + " :File");
+                else {
+                    System.out.println("File: " + s[i] );
                 }
             }
-
-        }else{
-            System.out.println(dir + ": not catalog");
+        }else {
+            System.out.println("Not Catalog: " + dir);
         }
 
 
