57640ea19edbd80051d3fc65c69e87d8858081a2
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-21 22:02:03 +0300

    seven

--- src/three/six.java
+++ src/three/six.java
@@ -1,16 +1,32 @@
 package three;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 public class six {
      public static void main(String...args){
 
          String s = "abcdefghjiklmnopqrstuxywvz";
          byte[] b = s.getBytes();
+         int c = 0;
 
-         ByteArrayInputStream one = new ByteArrayInputStream(b);
-         ByteArrayInputStream two = new ByteArrayInputStream(b,0,3);
-         System.out.println(b.length);
+
+         try( ByteArrayInputStream one = new ByteArrayInputStream(b);
+              ByteArrayInputStream two = new ByteArrayInputStream(b,3,3);){
+
+             do{
+                 for (int i = 0; i < b.length; i++){
+                     c = two.read();
+                     if(c != -1){
+                         System.out.print((char)c + " ");
+                     }
+                 }
+             }while (c != -1);
+
+         }catch (IOException e){
+            e.printStackTrace();
+         }
+
 
      }
 
