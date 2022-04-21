ba74777ff1b0dd47e6ceebcbbf06f01ac105df2b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-21 21:09:41 +0300

    three

---
+++ src/three/four.java
@@ -0,0 +1,41 @@
+package three;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class four {
+    public static void main(String...args) throws FileNotFoundException {
+        int size;
+
+        try(FileInputStream F = new FileInputStream("C:\\Users\\Max.000\\IdeaProjects\\Practice_Streams\\src\\three\\four.java")){
+            size =  F.available();
+
+            int n = size/40;
+            byte[] b = new byte[n];
+            System.out.println("Bytes available: " + F.available());
+
+            for (int i = 0; i < n; i++){
+                System.out.println((char)F.read());
+            }
+            System.out.println("\nStill available: "  + F.available());
+
+            if(F.read(b) != n){
+                System.err.println("Cant read: " + n + " bytes");
+            }
+            System.out.println(new String(b,0,n));
+            System.out.println("\nStill available: "  + F.available());
+            System.out.println("Skip last half bytes: " + F.skip(F.available()/2));
+            System.out.println("\nStill available: "  + F.available());
+
+            System.out.println("Read last bytes in arr ");
+            if(F.read(b,n/2,n/2) != n/2){
+                System.err.println("Cant read: " + n/2 + " bytes");
+            }
+            System.out.println("\nStill available: "  + F.available());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
--- src/three/three.java
+++ src/three/three.java
@@ -6,9 +6,13 @@ import java.io.FilenameFilter;
 public class three {
     public static void main(String...args){
         String dir = "C:\\Users\\Max.000\\IdeaProjects\\Practice_Streams\\src\\three";
+
         File f = new File(dir);
+
         FilenameFilter only = new Test("txt");
+
         String[] s = f.list(only);
+
         for (int x = 0 ; x <= s.length-1;x++){
 
             System.out.println(s[x]);
