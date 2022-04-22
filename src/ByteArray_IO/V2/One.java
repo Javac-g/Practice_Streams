de310aa02bc6ba4676475cfb4020e268fab1ed51
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-22 16:40:29 +0300

    five

--- src/ByteArray_IO/V2/One.java
+++ src/ByteArray_IO/V2/One.java
@@ -1,29 +1,33 @@
 package ByteArray_IO.V2;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 
 public class One {
     public static void main(String... args) throws IOException {
-
-        String s = "abcdefghkijlmnopqrstuxvwyz";
+        String s = "abcdefghijklmnopqrstuxvwyz";
         byte[] b = s.getBytes();
 
-        try(ByteArrayInputStream I = new ByteArrayInputStream(b)){
-            int c;
-            do{
-                c = I.read();
-                if(c != -1){
-                    System.out.println((c) + ": " + Integer.toBinaryString(c));
+        try(ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(b,0,3)) {
+
+            for (int i = 0; i < 2; i++){
+                int c;
+                while ((c = byteArrayInputStream.read()) != -1){
+
+                    if(i == 0){
+                        System.out.print((char)c);
+                    }else {
+                        System.out.print(Character.toUpperCase((char)c));
+                    }
+
                 }
-            }while (c != -1);
+                System.out.println();
+                byteArrayInputStream.reset();
 
+            }
 
-
-        }catch (IOException e){
-            e.printStackTrace();
         }
-
 
 
 
