1d37e864f191114f6dc9e82eea54df7b9d2f9229
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-29 23:32:19 +0300

    nine

--- src/Symbols_IO/CharArrayWriter/One.java
+++ src/Symbols_IO/CharArrayWriter/One.java
@@ -1,6 +1,7 @@
 package Symbols_IO.CharArrayWriter;
 
 import java.io.CharArrayWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class One {
@@ -29,6 +30,26 @@ public class One {
             System.out.print(temp[i] + " ");
         }
 
+
+        try(FileWriter fileWriter = new FileWriter("C:\\Users\\Max.000\\IdeaProjects\\Practice_Streams\\src\\Symbols_IO\\CharArrayWriter\\a.txt")){
+
+            charArrayWriter.writeTo(fileWriter);
+
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+
+        }
+        charArrayWriter.reset();
+
+        System.out.println("\n");
+
+        for (int i = 0; i < 3; i++ ){
+
+            charArrayWriter.write('X');
+        }
+        System.out.println(charArrayWriter.toString());
 
 
 
