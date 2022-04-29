eeee5b508a6e7f1bf47db2f4d093a60c57c833f1
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-29 20:50:26 +0300

    three

---
+++ src/PrintStream/One.java
@@ -0,0 +1,18 @@
+package PrintStream;
+
+import java.io.PrintStream;
+
+public class One {
+
+    public static void main(String...args){
+
+
+        PrintStream A = new PrintStream(System.out,true);
+        A.println("hello");
+
+
+
+
+    }
+
+}
