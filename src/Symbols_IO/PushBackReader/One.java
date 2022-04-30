3407bc83d94c94d59eb227cafafba0ab4f6390a5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-30 21:11:28 +0300

    two

--- src/Symbols_IO/PushBackReader/One.java
+++ src/Symbols_IO/PushBackReader/One.java
@@ -1,9 +1,51 @@
 package Symbols_IO.PushBackReader;
 
+import java.io.CharArrayReader;
+import java.io.IOException;
+import java.io.PushbackInputStream;
+import java.io.PushbackReader;
+
 public class One {
 
     public static void main(String...args){
+        String s = "One two threooe four five";
+        char[] c = new char[s.length()];
 
+        s.getChars(0,s.length(),c,0);
+        CharArrayReader charArrayReader = new CharArrayReader(c);
+
+        int o;
+
+        try(PushbackReader pushbackReader = new PushbackReader(charArrayReader)){
+
+            while ((o = pushbackReader.read()) != -1){
+
+                switch (o){
+                    case 'o' ->{
+                        if((o = pushbackReader.read()) == 'o'){
+                            System.out.print('q');
+                        }else {
+                            System.out.print('*');
+                            pushbackReader.unread(o);
+                        }
+                        break;
+
+                    }
+                    default -> {
+                        System.out.print((char) o);
+                        break;
+                    }
+                }
+
+
+            }
+
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+
+        }
 
 
 
